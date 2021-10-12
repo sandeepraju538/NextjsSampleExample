4e05f1f7f24539843661b9acc0722ce4974b35a4
Sandeep Raju K <sandeepraju538@gmail.com>
2021-10-12 13:47:44 +0200

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,25 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
+  <Layout title="Home | Next.js + TypeScript Example">{
+    <style jsx global>{`
+      body {
+        margin: 0;
+        padding: 0;
+        font-size: 15px;
+        font-weight: 400;
+        line-height: 1.8;
+        color: #333;
+        font-family: sans-serif;
+      }
+      h1 {
+        font-weight: 700;
+      }
+      p {
+        margin-bottom: 10px;
+      }
+    `}</style>
+  }
     <h1>Hello Sandeep 👋</h1>
     <p>
       <Link href="/about">
